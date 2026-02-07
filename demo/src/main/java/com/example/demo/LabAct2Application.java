b697e2e952034e18d54a02d6f6006ddb1275a94b
Eron Asia <eronasia@gmail.com>
2026-02-07 15:10:59 +0800

    Commited the parts I was unable to upload to GitHub due to the file size limit.

---
+++ demo/src/main/java/com/example/demo/LabAct2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabAct2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabAct2Application.class, args);
+	}
+
+}
---
+++ demo/src/main/java/com/example/demo/Model/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.Model;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long>{
+    
+    Optional<UserModel> findByUsername(String username);
+    
+    UserModel findByEmail(String email);
+    
+}
